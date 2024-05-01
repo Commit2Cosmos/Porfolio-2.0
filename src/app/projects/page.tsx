74b8ceab68e031652c8077e71152ddfaa61fad8a
Anton Belov <anton.belov.al7@gmail.com>
2024-05-01 16:15:36 +0100

    button placed

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -12,19 +12,19 @@ export const metadata: Metadata = {
 
 export default function Projects() {
     return (
-        <main className="relative
-        w-screen h-screen
-        ">
-            <section className="outline-all w-full h-full
+        <main className="relative w-screen h-screen overflow-hidden">
+            <section className="w-full h-full
             flex
             ">
                 <ProjectsSections />
             </section>
-            <div className='absolute'>
-                <HiChevronDoubleUp size='55' className='chevron-btn' />
-                <Link href={paths.other}>
-                    Other
-                </Link>
+            <div className='absolute center-btn bottom-0'>
+                <div className='outline-all'>
+                    <HiChevronDoubleUp size='55' className='chevron-btn hover:grayscale-0 duration-200' />
+                    <Link href={paths.other} className='text-center min-w-20 absolute center-btn'>
+                        <p>Other</p>
+                    </Link>
+                </div>
             </div>
         </main>
     );
