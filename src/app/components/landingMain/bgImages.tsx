b32c90fb94be553c6293efb94d3c00928002e48c
Anton Belov <anton.belov.al7@gmail.com>
2024-05-10 14:48:46 +0100

    galaxy image faded

--- src/app/components/landingMain/bgImages.tsx
+++ src/app/components/landingMain/bgImages.tsx
@@ -2,13 +2,24 @@ import Image from "next/image"
 
 export default function() {
     return (
-        <div className='absolute z-10 bottom-0 right-[40%]'>
-            <Image
-            src={"/telescope.png"}
-            alt='telescope image'
-            width={140}
-            height={1}
-            />
-        </div>
+        <>
+            <div className='absolute z-10 bottom-0 right-[40%]'>
+                <Image
+                src={"/telescope.png"}
+                alt='telescope image'
+                width={180}
+                height={1}
+                />
+            </div>
+            <div className='absolute z-10 bottom-[10%] right-[5%] hidden'>
+                <Image
+                src={"/night-sky.jpg"}
+                alt='galaxy image'
+                width={400}
+                height={1}
+                />
+                <div className="img-fade"></div>
+            </div>
+        </>
     )
 }
